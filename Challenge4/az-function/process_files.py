1a0cc34ee2c924c8b51cfe5bc7375a9bd285d5f0
cons-jfesta <j.festa@consulcesi.com>
2024-12-13 15:09:40 +0000

    function fixes and challenge3 impl

--- Challenge4/az-function/process_files.py
+++ Challenge4/az-function/process_files.py
@@ -7,6 +7,8 @@ from azure.storage.blob import BlobServiceClient, generate_blob_sas, BlobSasPerm
 from azure.core.credentials import AzureKeyCredential
 from azure.ai.documentintelligence import DocumentIntelligenceClient
 from azure.ai.documentintelligence.models import AnalyzeResult, AnalyzeDocumentRequest
+from azure.ai.formrecognizer import DocumentAnalysisClient as OldDocumentIntelligenceClient, AnalyzeResult as OldAnalyzeResult
+import httpx
 
 def get_blob_service_client():
     return BlobServiceClient.from_connection_string(os.getenv('STORAGE_CONNECTION_STRING'))
@@ -41,16 +43,16 @@ def generate_sas_url(blob_service_client, container_name, blob_name, expiry_hour
 
 def analyze_layout(sas_url):
 
-    document_intelligence_client = DocumentIntelligenceClient(
+    document_intelligence_client = OldDocumentIntelligenceClient(
         endpoint=os.getenv("DOCUMENTINTELLIGENCE_ENDPOINT"),
         credential=AzureKeyCredential(os.getenv("DOCUMENTINTELLIGENCE_API_KEY")),
     )
 
     poller = document_intelligence_client.begin_analyze_document(
-        "prebuilt-layout", AnalyzeDocumentRequest(url_source=sas_url)
+        "prebuilt-layout", httpx.Client().get(sas_url).read()
     )
 
-    result: AnalyzeResult = poller.result()
+    result: OldAnalyzeResult = poller.result()
 
     analysis_result = {
         "handwritten": (
@@ -144,7 +146,7 @@ def save_analysis_results(
     results_blob_name = f"{blob_name}_results.json"
 
     # Convert the analysis results to JSON
-    results_json = json.dumps(analysis_results, indent=2)
+    results_json = json.dumps(analysis_results, indent=4)
 
     # Upload the results to the blob
     blob_client = blob_service_client.get_blob_client(
